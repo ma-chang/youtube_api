7be828a1457fcf130f42e0ba5c63d2120ed40888
ma-chang <akihisa.bamba.10@gmail.com>
2022-03-17 23:27:06 +0900

    set up youtube_api/urls

--- youtube_api/urls.py
+++ youtube_api/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from socket import fromfd
+from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('authen/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_)
