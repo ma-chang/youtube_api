956ad5210d75bddf773a4a2078b0b95e876ca5f8
ma-chang <akihisa.bamba.10@gmail.com>
2022-03-19 00:34:30 +0900

    implement database model

--- youtube_api/urls.py
+++ youtube_api/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('authen/', include('djoser.urls.jwt')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
