1ae634dc690b7c68dc3afcb715a5eb7d5d37acdb
ma-chang <akihisa.bamba.10@gmail.com>
2022-03-19 11:30:36 +0900

    implement CreateUserView and VideoViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import generics
+from .serializers import UserSerializer, VideoSerializer
+from rest_framework.permissions import AllowAny
+from .models import Video
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializers_class = UserSerializer
+    permissions_classes = (AllowAny,)
+
+
+class VideoViewSet(generics.ModelViewSet):
+    queryset = Video.objects.all()
+    serializers_class = VideoSerializer
