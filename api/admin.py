6ab67b57c815d66937a57fa91b80c35444b0fc9e
ma-chang <akihisa.bamba.10@gmail.com>
2022-03-19 09:06:56 +0900

    overrided UserAdmin model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Video, User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "username"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("username",)}),
+        (_("Permission"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Inportant_dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
+    )
+
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Video)
