30d53a1d0658c3aca002d42d9d36f306298afdb4
ma-chang <akihisa.bamba.10@gmail.com>
2022-03-19 11:16:20 +0900

    implement user and video serializer

---
+++ api/serializers.py
@@ -0,0 +1,24 @@
+from pyexpat import model
+import re
+from this import d
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import User, Video
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta(self):
+        model = get_user_model()
+        fields = ("email", "password", "username", "id")
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(validated_data)
+
+        return user
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta(self):
+        model = Video
+        fields = ["id", "title", "video", "thumb", "like", "dislike"]
