4ef716625c9b65c5c3d67222e4038633f61402e6
ma-chang <akihisa.bamba.10@gmail.com>
2022-04-15 08:56:07 +0900

    implement env file and re-generated secret key

--- youtube_api/settings.py
+++ youtube_api/settings.py
@@ -10,18 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import environ
+
 from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Read env file
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, ".env"))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%qyn_nj(3&nl#r^$gss^a+z*3%q2_*$!no0xs)3ca7mvrvtz-1"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
